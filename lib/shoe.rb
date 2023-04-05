2b424f70396d723e6dfd193d1653cb42e37b1552
Lluvia <friasllu@gmail.com>
2023-04-05 11:16:53 -0400

    commit tree f7dd6b6d22557c428edaac13ca989a152a2149e0

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -2,9 +2,16 @@ class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+  
+    BRANDS << brand unless BRANDS.include?(brand)
   end
+
+  
+ 
 
   def cobble
     self.condition = "new"
